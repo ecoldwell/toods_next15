9c1c4ee7f45c7d6da124af67632e02b557f4d7ad
ecoldwell <emmalcoldwell@gmail.com>
2025-03-20 16:31:08 -0400

    cleaned homepage

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -26,27 +26,7 @@ export default async function Page() {
     content={page?.homePage.content} />
   ) : (
     <section className="container mx-auto grid grid-cols-1 gap-6 p-12">
-      <Title>Toodinator</Title>
-      {/* <Menu></Menu> */}
-      <Navigation></Navigation>
-      <Link href="/posts">Posts index &rarr;</Link>
-      <ul className="grid grid-cols-1 gap-12 lg:grid-cols-2">
-        {events.map((media: { _id: Key | null | undefined; slug: { current: any; }; name: string | number | bigint | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<AwaitedReactNode> | Iterable<ReactNode> | null | undefined; date: string | number | Date; }) => (
-          <li className="bg-white p-4 rounded-lg" key={media._id}>
-            <Link
-              className="hover:underline"
-              href={`/events/${media?.slug?.current}`}
-            >
-              <h2 className="text-xl font-semibold">{media?.name}</h2>
-              {media?.date && (
-                <p className="text-gray-500">
-                  {new Date(media.date).toLocaleDateString()}
-                </p>
-              )}
-            </Link>
-          </li>
-        ))}
-      </ul>
+
     </section>
   )
 }
