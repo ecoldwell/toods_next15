4c8eb8746b746c81abf6181bc5a033379dcb5a6b
ecoldwell <emmalcoldwell@gmail.com>
2026-09-03 08:33:24 -0400

    update sanity, colour-input plugin

---
+++ src/components/PostMasonry.tsx
@@ -0,0 +1,40 @@
+// components/PostsMasonry.tsx
+"use client";
+
+import { PostCard } from "@/components/PostCard";
+import Masonry from 'react-masonry-css';
+
+type Post = {
+  _id: string;
+  [key: string]: unknown;
+};
+
+interface PostsMasonryProps {
+  posts: Post[];
+}
+
+const breakpointColumnsObj = {
+  default: 3,
+  // 1700: 3,
+  1500: 2,
+  700: 1,
+  500: 1
+};
+
+export function PostsMasonry({ posts }: PostsMasonryProps) {
+  return (
+  <main className = "masonry-container-wrapper">
+ <Masonry
+  breakpointCols={breakpointColumnsObj}
+  className="masonry-container"
+  columnClassName="masonry-column"
+>
+    <div className="masonry-item">
+      {posts.map((post) => (
+        <PostCard key={post._id} {...post} />
+      ))}
+        </div>
+      </Masonry>
+  </main>
+  );
+}
